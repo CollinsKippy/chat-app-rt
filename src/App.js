307e057c58b1185f5b47bc49128ac30b7f2d0739
CK <collins@analence.com>
2023-01-17 21:42:46 +0300

    apply chat provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import ChatList from './components/ChatList';
 import Navbar from './components/Navbar';
+import { ChatContextProvider } from './contexts/ChatContext';
 
 function App() {
   return (
-    <>
+    <ChatContextProvider>
       <Navbar />
       <ChatList />
-    </>
+    </ChatContextProvider>
   );
 }
 
